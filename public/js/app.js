334ab2068b102b7fb3731757ad5a465cc8f941ff
Garry Knight <gazzaknight@live.com>
2021-01-30 21:16:34 +0000

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -5,9 +5,6 @@ fetch('http://puzzle.mead.io/puzzle').then((response)=>{
     })
 })
 
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -20,7 +17,7 @@ weatherForm.addEventListener('submit',(e)=>{
     const location = search.value
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
+    fetch(`/weather?address=${location}`).then((response)=>{
         response.json().then(({error, location, forecast})=>{
             if(error){
                return messageOne.textContent = error
